fb4d2fa74e50f0654dc789f90b61bfdd82d9a3d0
christian rochford <rochfordchristian@gmail.com>
2013-11-20 17:09:36 +0000

    add error messages

--- process.php
+++ process.php
@@ -48,11 +48,7 @@ if( isset($_POST) ){
 
 //send email if all is ok
 if($formok){
-    $headers = "From: Your website contact form<script type="text/javascript">
-/* <![CDATA[ */
-(function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName("script");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})();
-/* ]]> */
-</script>" . "\r\n";
+    $headers = "From: user@website.com" . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     
     $emailbody = "<p>You have recieved a new message from the contact form on your website.</p>
@@ -87,6 +83,6 @@ if(empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUE
     $_SESSION['cf_returndata'] = $returndata;
     
     //redirect back to form
-    header('location: ' . $_SERVER['HTTP_REFERER']);
+    header('location: ' . $_SERVER['HTTP_REFERER']);  
 
 }
